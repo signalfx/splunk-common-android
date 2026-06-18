dac1aa45a8357ad646ce76ce4dd4c9839dcad9c8
Ondrej Synak <78600249+OndrejSynakSL@users.noreply.github.com>
2026-06-18 10:25:07 +0200

    DEMRUM-5699: Move common functionality from session replay repository (#10)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,7 +2,7 @@ object Dependencies {
 
     private const val gradleVersion = "7.3.1"
     private const val kotlinVersion = "1.7.20"
-    private const val ktlintVersion = "1.7.1"
+    private const val ktlintVersion = "1.8.0"
     private const val jacocoVersion = "0.8.14"
 
     const val gradle = "com.android.tools.build:gradle:$gradleVersion"
@@ -28,7 +28,7 @@ object Dependencies {
     }
 
     object Android {
-        private const val annotationVersion = "1.9.1"
+        private const val annotationVersion = "1.6.0"
         private const val recyclerVersion = "1.2.1"
         private const val materialVersion = "1.9.0"
 
